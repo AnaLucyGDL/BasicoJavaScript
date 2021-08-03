b024c88f843015ebf789b7e0393812334b6adee8
Lucy Guzmán <analucygdl@gmail.com>
2021-08-03 14:51:10 -0500

    Generación de una variable

--- variables.js
+++ variables.js
@@ -1,2 +1,5 @@
 //¿Qué es una variable? Guardar valores en memoria. 
 //Representación de algún lugar en memoria reservado para un valor.
+
+//Variable llamada "nombre" con el valor "Lucy" que puede ser llamada al ser necesitada
+var nombre = "Lucy";
