8917dcdd20245eae00f02151e7c8d86b8987fd0c
Lucy Guzmán <analucygdl@gmail.com>
2021-08-04 17:45:15 -0500

    Alcances fuera/dentro de cada scope

--- scope.js
+++ scope.js
@@ -5,7 +5,13 @@
 var nombre = "Lucy";
 
 //Scope Local
+//Función que genera una variable internat llamada apellido.
 function fun(){
     var apellido = "Guzmán";
     return nombre + "" + apellido
+    //console.log(apellido); Puede dar resultado al estar en el scope local junto a la variable que necesita
+    //Un scope local tiene acceso a las variables del scope global, pero el global no tiene
+    //acceso a las variables de un scope local
 }
+
+ //console.log(apellido); No puede dar resultando al estar fuera del scope local
