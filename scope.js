e9af597c84f63db928004dd871a96f6877076eb5
Lucy Guzmán <analucygdl@gmail.com>
2021-08-04 17:31:36 -0500

    Tipos de scope

--- scope.js
+++ scope.js
@@ -1,3 +1,11 @@
 //Scope: Alcance que tienen las variables. 
 //Depende mucho donde declares e inicialices una variable si se tiene acceso a esta o no
 
+//Scope Global
+var nombre = "Lucy";
+
+//Scope Local
+function fun(){
+    var apellido = "Guzmán";
+    return nombre + "" + apellido
+}
