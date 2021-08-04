e6e6ef99b85a0502697cc419e441d5d4db2ae491
Lucy Guzmán <analucygdl@gmail.com>
2021-08-03 23:58:48 -0500

    Función declarativa

---
+++ funciones.js
@@ -0,0 +1,9 @@
+//Declarativas
+//•Al usarse una palabra reservada "function" se está declarando
+//•Con el nombre de la función se reserva un espacio en memoria para... 
+//guardar el calculo o la operación que se hace dentro de la función.
+function miFunción(){
+    return 3;
+}
+
+//De expresión
