a8d7b02ad043e3e8a5e4c5e48e997451d3be7ac7
Lucy Guzmán <analucygdl@gmail.com>
2021-08-04 00:41:57 -0500

    Prueba en terminal del navegador

--- ejemplosFunciones.js
+++ ejemplosFunciones.js
@@ -1,4 +1,9 @@
-//Creo una función declarativa con el nombre "saludo" cuyo parametro es estudiante
+//Creo una función declarativa con el nombre "saludo" cuyo parametro es "estudiante"
+//una variable sin valor definido, pues se le da después.
 function saludo (estudiante){
-    console.log(estudiante);//Función para imprimir cosas en la consola
+    console.log(estudiante);//Función para imprimir cosas en la consola del navegador
 }
+//Se llama a la función y se le da valor a la variable "estudiante"
+saludo("Lucy");
+
+//Al correrse en la terminal del navegador, se muestra el valor dado a "estudiante"
