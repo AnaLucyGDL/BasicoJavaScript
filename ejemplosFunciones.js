caafe13ed327b377de4e65efbe7a248e023592d8
Lucy Guzmán <analucygdl@gmail.com>
2021-08-04 00:36:11 -0500

    Creación de función para imprimir en consola

---
+++ ejemplosFunciones.js
@@ -0,0 +1,4 @@
+//Creo una función declarativa con el nombre "saludo" cuyo parametro es estudiante
+function saludo (estudiante){
+    console.log(estudiante);//Función para imprimir cosas en la consola
+}
