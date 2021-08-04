e95cf93df1cea5d9045f3ef9bbcf66a8f405394d
Lucy Guzmán <analucygdl@gmail.com>
2021-08-03 23:40:38 -0500

    Variable tipo objeto

--- variables.js
+++ variables.js
@@ -4,7 +4,7 @@
 //Variable llamada "nombre" con el valor "Lucy" que puede ser llamada al ser necesitada
 var nombre = "Lucy";
 
-//Declarar. Despúes se le dará un valor.
+//Declarar. Después se le dará un valor.
 var edad;
 
 edad //Indefinida. Espacio reservado sin valor, aún.
@@ -20,3 +20,13 @@ var elementos = ["Computadora","Celular"];
 //"Elementos" es el espacio designado en memoria, tiene valores string/cadena de caracteres.
 //[] para valores string.
 //Simbolo "=" para inicializar
+
+//Variable tipo "objeto"
+//Objeto/Variable compleja porque guarda más valores de diferentes tipos
+var persona = {
+    nombre : "Lucy", //coma para separar 
+    edad : 20
+}
+
+//Para mandar llamar al objeto con sus valores ya establecidos:
+persona;
