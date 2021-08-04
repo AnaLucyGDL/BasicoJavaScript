88830e8414c93ad0b4a5dd377e518c2f22b2f15f
Lucy Guzmán <analucygdl@gmail.com>
2021-08-04 00:23:15 -0500

    Llamar funciones

--- funciones.js
+++ funciones.js
@@ -6,9 +6,19 @@ function miFunción(){
     return 3;
 }
 
+//Para llamar una función declarativa
+miFunción();
+
+
 //De expresión
 //•Las variables pueden guardar varios tipos de valores, incluso funciones.
-var miFunción = function (){
+//•Una función necesita parametros "()"
+//•Ponemos los parametros que se esperan recibir como valor cuando se llame 
+//la función para que esta pueda hacer algo
+var miFunción = function (a,b){
     return a + b; 
     //a y b serían el valor de dos variables que llamamos desde fuera de la función.
 }
+//Para llamar una función expresiva se usa el nombre de la variable que
+//se le dio, y al poner parentesis se trata como una función.
+miFunción();
