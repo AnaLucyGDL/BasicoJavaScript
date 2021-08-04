dd4902d75099bce3454e2d1535fdef0c56841e18
Lucy Guzmán <analucygdl@gmail.com>
2021-08-04 14:25:37 -0500

    Ejemplo con base de datos existente

--- ejemplosFunciones.js
+++ ejemplosFunciones.js
@@ -1,9 +1,12 @@
 //Creo una función declarativa con el nombre "saludo" cuyo parametro es "estudiante"
 //una variable sin valor definido, pues se le da después.
 function saludo (estudiante){
-    console.log(estudiante);//Función para imprimir cosas en la consola del navegador
+    console.log(`Hola ${estudiante}`);
+    //console.log = Función para imprimir cosas en la consola del navegador
+    //(`Hola ${estudiante}`) Para conectar a una base de datos con los nombres que son asignados a "estudiante"
 }
 //Se llama a la función y se le da valor a la variable "estudiante"
 saludo("Lucy");
 
 //Al correrse en la terminal del navegador, se muestra el valor dado a "estudiante"
+
