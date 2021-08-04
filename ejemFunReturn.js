f458bed84605b38aa9f86c6c5611449b9a52cd58
Lucy Guzmán <analucygdl@gmail.com>
2021-08-04 17:17:59 -0500

    Contenido de función sin valores establecidos

---
+++ ejemFunReturn.js
@@ -0,0 +1,10 @@
+//Para que un valor sea dado sin usar console.log
+
+//Función declarativa llamada "sumar" cuyos parametros son a y b
+//Contiene una variable. El valor de esta es el resultado de sumar a y b (aún no tienen valor)
+function sumar(a,b){
+    var resultado = a + b;
+    return resultado;
+    //return: palabra reservada para regresar valores.
+}
+
