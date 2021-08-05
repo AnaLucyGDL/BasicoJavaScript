5d8e542acd2a6a90fa06f7f23e46724da1bb66e5
Lucy Guzmán <analucygdl@gmail.com>
2021-08-05 18:07:01 -0500

    Ejemplo *

--- coercion.js
+++ coercion.js
@@ -7,6 +7,7 @@
 2 + true; //3
 false - 3; //-3
 
+//Implicitas
 //En terminal de navegador:
 var a = 4 + "7";
 typeof a
@@ -14,3 +15,9 @@ typeof a
 //el simbolo "+" se usa para concatenar
 //JavaScript entiende 4 + "7" como una concatenación, por lo que da un resultado de 47, convirtiendo
 //el 7 en un string
+
+var b = 4 * "7";
+typeof b
+//b muestra que contiene un valor "number" (numerico)
+//el simbolo "*" es un operador, por lo que el codigo supone que el "7" debe ser y es un valor
+//numerico con el que se hace una operación matematica, resultando en "28"
