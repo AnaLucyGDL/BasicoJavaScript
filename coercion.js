c0973f0d166d64ea98db102de736480d68af6b06
Lucy Guzmán <analucygdl@gmail.com>
2021-08-05 16:03:28 -0500

    Tipo de dato

--- coercion.js
+++ coercion.js
@@ -1,5 +1,8 @@
 //Coerción: Forma en que se puede cambiar un tipo de valor a otro
-
 //Coerciones implicitas: Cuando el lenguaje ayuda y cambia un tipo de valor a otro
-
 //Coerciones explicitas: Forma en que obligamos que un valor de un tipo cambie a otro
+
+//En terminal de navegador:
+var a = 4 + "7";
+typeof a
+//a muestra que contiene un valor "string"
