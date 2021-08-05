70488ff94a537f9b1d04d8fa8a45e21bfc4ab50c
Lucy Guzmán <analucygdl@gmail.com>
2021-08-05 15:24:35 -0500

    Valor indefinido de variables, explicaicón

--- hoisting.js
+++ hoisting.js
@@ -1,7 +1,11 @@
 //Hoisting: Cuando las variables y funciones se declaran antes de que se procese codigo
 
-//¿Qu´pe sucedería si se pide imprimir el valor de la variable antes de declarar e inicializar la misma?
+//¿Qué sucedería si se pide imprimir el valor de la variable antes de declarar e inicializar la misma?
 
 console.log(nombre);
 
 var nombre = "Lucy";
+
+//La terminal del navegador indica que el valor pedido está indefinido
+//Cuando el codigo se esta procesando en el navegador, se lee que hay una variable, pero al no
+//encontrar su declaración, lo pone por default como "undefined/indefinido"
