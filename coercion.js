b42e68b7c1ce5392114a3528bceb0543ba8e0114
Lucy Guzmán <analucygdl@gmail.com>
2021-08-05 18:02:55 -0500

    Ejemplo concatenación

--- coercion.js
+++ coercion.js
@@ -2,7 +2,15 @@
 //Coerciones implicitas: Cuando el lenguaje ayuda y cambia un tipo de valor a otro
 //Coerciones explicitas: Forma en que obligamos que un valor de un tipo cambie a otro
 
+4 + "7"; //47
+4 * "7"; //28
+2 + true; //3
+false - 3; //-3
+
 //En terminal de navegador:
 var a = 4 + "7";
 typeof a
 //a muestra que contiene un valor "string"
+//el simbolo "+" se usa para concatenar
+//JavaScript entiende 4 + "7" como una concatenación, por lo que da un resultado de 47, convirtiendo
+//el 7 en un string
