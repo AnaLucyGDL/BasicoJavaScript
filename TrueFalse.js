c57f790bb3f6304978be78621d7635e0f211ad36
Lucy Guzmán <analucygdl@gmail.com>
2021-08-05 18:42:57 -0500

    Ejemplos True

--- TrueFalse.js
+++ TrueFalse.js
@@ -6,4 +6,13 @@ Boolean();
 Boolean(0);
 Boolean(undefined);
 Boolean(false);
-Boolean("");
+Boolean("");//vacio
+
+//Verdaderos verdaderos
+Boolean(" ");//con un caracter
+Boolean("a");//caracteres
+Boolean(1); //cualquier numero salvo 0
+Boolean([]);//arrays true por default aunque este vacio
+Boolean({});//objeto true por default ""
+Boolean(function(){});//true por default ""
+Boolean(true);
