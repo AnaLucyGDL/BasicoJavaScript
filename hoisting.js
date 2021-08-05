66238a170ce1eac7b90ed834d12a140bc1c2b359
Lucy Guzmán <analucygdl@gmail.com>
2021-08-05 15:12:38 -0500

    Prueba en navegador

--- hoisting.js
+++ hoisting.js
@@ -1,1 +1,7 @@
 //Hoisting: Cuando las variables y funciones se declaran antes de que se procese codigo
+
+//¿Qu´pe sucedería si se pide imprimir el valor de la variable antes de declarar e inicializar la misma?
+
+console.log(nombre);
+
+var nombre = "Lucy";
