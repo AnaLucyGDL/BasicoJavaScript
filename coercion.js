eacc2e3fbf0d77cba6045738665a485568e7d534
Lucy Guzmán <analucygdl@gmail.com>
2021-08-05 15:58:11 -0500

    Conceptos

---
+++ coercion.js
@@ -0,0 +1,5 @@
+//Coerción: Forma en que se puede cambiar un tipo de valor a otro
+
+//Coerciones implicitas: Cuando el lenguaje ayuda y cambia un tipo de valor a otro
+
+//Coerciones explicitas: Forma en que obligamos que un valor de un tipo cambie a otro
