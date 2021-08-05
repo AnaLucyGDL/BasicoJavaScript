280ee9d2013e0a317500a13fbd2eac6c05317a31
Lucy Guzmán <analucygdl@gmail.com>
2021-08-05 15:30:09 -0500

    2da prueba en terminal de navegador

--- hoisting.js
+++ hoisting.js
@@ -3,9 +3,20 @@
 //¿Qué sucedería si se pide imprimir el valor de la variable antes de declarar e inicializar la misma?
 
 console.log(nombre);
-
 var nombre = "Lucy";
 
 //La terminal del navegador indica que el valor pedido está indefinido
 //Cuando el codigo se esta procesando en el navegador, se lee que hay una variable, pero al no
 //encontrar su declaración, lo pone por default como "undefined/indefinido"
+
+//Se llama una función antes de que la creemos, y dentro de esta se pide que la consola imprima
+//un saludo junto al valor de una variable que es declarada despúes fuera de la propia función
+
+hey();
+function hey(){
+    console.log("Hola " + nombre);
+}
+var nombre = "Lucy";
+
+//Al hacerse la prueba en la terminal del navegador, la función se ejecuta correctamente, pero
+//el valor se muestra como indefinido de nuevo.
