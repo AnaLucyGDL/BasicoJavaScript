25a1ddd095b70a9816cab1addbf416151df2fa85
Lucy Guzmán <analucygdl@gmail.com>
2021-08-06 16:52:01 -0500

    Comparación =, <, >

--- operadores.js
+++ operadores.js
@@ -11,3 +11,25 @@
 //como string igualmente
 
 !false //! significa negación (En este caso se nega que algo es falso, dando como resultado true en la consola)
+
+var a = 1; //declaracipon de una variable en que a es igual (=) a 1.
+
+3 == "3"; 
+// == (dos iguales) se usa para comparar. No compara el tipo de dato, sino su valor. En este caso, resultará en
+//true al ser dos valores iguales pero de diferentes tipos.
+
+3 === "3";
+// === (tres iguales) se utiliza para validar que lo comparado es estrictamente igual tanto de
+//tipo como de su valor. En este caso da un resultado "falso" ya que un dato es numerico y otro string
+
+5 < 3; // < (menor que...). False, porque 5 no es menor que 3.
+
+5 > 3; // > (mayor que...). True, al ser 5 mayor que 3.
+
+5 <= 6; // <= (menor o igual que...). True, 5 es menor que 6.
+//validación de dos pasos. ¿Es menor? Sí ¿Es igual? No, pero la primer validación es verdadera.
+
+5 >= 6; // >= (mayor o igual que...). False, 5 no es ni mayor ni igual que 6.
+
+
+
