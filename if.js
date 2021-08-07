9de70da0db62bf26c7af9aa061e5bed253a13447
Lucy Guzmán <analucygdl@gmail.com>
2021-08-06 19:17:30 -0500

    Prueba en consola de navegador: true

--- if.js
+++ if.js
@@ -1,6 +1,9 @@
 //Las condicionales son las reglas que se aplicaran para validar si algo es verdadero o falso
 //y que se puedan generar acciones con eso
 
+//condicional con validación true o false
 if (true){
-
+    console.log("Hola");
+} else {
+    console.log("Soy falso");
 }
