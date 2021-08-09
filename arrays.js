9afcee1aef82f68223977e8bf2d38df4d7b6c6aa
Lucy Guzmán <analucygdl@gmail.com>
2021-08-09 18:50:47 -0500

    Agregar un elemento

--- arrays.js
+++ arrays.js
@@ -13,4 +13,12 @@ console.log (frutas.length);
 //Para acceder a cada elemento del array
 console.log (frutas[0]); //en programación, las cuentas inician desde 0
 //Al introducir lo anterior en la consola del navegador, regresara "Manzana" al ser el elemento
-//en la posición señalada (0), si se introduce 1, se mostrará "Plátano", y así sucesivamente.
+//en la posición señalada (0) del index, si se introduce 1, se mostrará "Plátano", y así sucesivamente.
+
+//Para mutar el array (realizar cambios con metodos)
+                       //push va a añadir elementos al array
+var masFrutas = frutas.push("Uvas");
+console.log (frutas);
+//Se mostraran los primeros 4 elementos, y se le agregará el quinto recién agregado (Uvas), mostrando
+//que ahora hay 5 elementos en total.
+console.log (frutas[0]);//para acceder al elemento "Uvas"
