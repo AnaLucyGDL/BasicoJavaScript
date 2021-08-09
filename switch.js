4ca83309ccfcac580724ffb6ea9180c57c8ec884
Lucy Guzmán <analucygdl@gmail.com>
2021-08-09 15:03:12 -0500

    Ejemplo de uso de casos

---
+++ switch.js
@@ -0,0 +1,11 @@
+//El switch es una forma de generar una condicion y validar cosas, pero este funciona por casos.
+//Si un caso es verdad, se ejecuta esa parte, si no, se pasa a otros casos. Nuestro "else" es "default"
+var numero = 1;
+switch (numero){
+    case 1:
+        console.log("Es uno");
+        break; //ya no deja que pase a los otros casos
+    case 10:
+        console.log("Es diez");
+        break;
+}
