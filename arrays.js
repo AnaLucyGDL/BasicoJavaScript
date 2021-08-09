2de4b9110a03c1788a5337a1af8f81e9a4a2e0a8
Lucy Guzmán <analucygdl@gmail.com>
2021-08-09 18:07:19 -0500

    Acceder a cierto elemento del array

--- arrays.js
+++ arrays.js
@@ -10,3 +10,7 @@ console.log (frutas);
 console.log (frutas.length);
 //La consola regresara que hay 4 elementos dentro
 
+//Para acceder a cada elemento del array
+console.log (frutas[0]); //en programación, las cuentas inician desde 0
+//Al introducir lo anterior en la consola del navegador, regresara "Manzana" al ser el elemento
+//en la posición señalada (0), si se introduce 1, se mostrará "Plátano", y así sucesivamente.
