c7fbdc6ff28ba20045f2d33b9a165c44b4b87587
Lucy Guzmán <analucygdl@gmail.com>
2021-08-09 17:20:25 -0500

    Ejemplo y prueba en navegador

--- arrays.js
+++ arrays.js
@@ -1,2 +1,6 @@
 //Estructura de datos y tipo objeto. Valor que va a guardar más valores adentro 
 //(numeros, strings, objetos)
+
+var fruras = ["Manzana", "Plátano", "Cereza", "Fresa"];
+
+console.log (frutas);
