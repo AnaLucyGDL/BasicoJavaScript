b586be50c2e5e2a420cde94c874533b46135ac01
Lucy Guzmán <analucygdl@gmail.com>
2021-08-09 20:31:28 -0500

    Creación de array y función

--- loops.js
+++ loops.js
@@ -1,3 +1,7 @@
 //Un loop es un bucle con el que una función puede puede seguir pasando mientras su condición se cumpla
 
 var estudiantes = ["Lucy", "Bety", "Ale", "Janie"];
+
+function saludarEstudiante(estudiantes){
+    console.log(`Hola ${estudiantes}`)
+}
