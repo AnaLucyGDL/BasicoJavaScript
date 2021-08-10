c0e110ca6747cbb33d1ce668c00fda7295f6bb4c
Lucy Guzmán <analucygdl@gmail.com>
2021-08-09 20:55:59 -0500

    Creación de bucle (for)

--- loops.js
+++ loops.js
@@ -2,6 +2,14 @@
 
 var estudiantes = ["Lucy", "Bety", "Ale", "Janie"];
 
-function saludarEstudiante(estudiantes){
-    console.log(`Hola ${estudiantes}`)
+function saludarEstudiantes(estudiante){
+    console.log(`Hola ${estudiante}`);
+}
+//Se está creando una variable que es igual a 0 (var i = 0;) 
+//La función se ejecutará mientras (;) i sea menor a la longitud de mi array de estudiantes, y en cada
+//bucle se le sumará uno a i
+for(var i = 0; i < estudiantes.length; i++){
+    saludarEstudiante(estudiantes[i]);
+//se llama a esta función, que mostrará el saludo con el nombre del almuno segur la posición
+//del index basado en i
 }
