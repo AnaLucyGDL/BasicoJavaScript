a05a22383dc5062871e0526691ce570ba10e46db
Lucy Guzmán <analucygdl@gmail.com>
2021-08-10 00:54:00 -0500

    Modo alterno del for

--- loops.js
+++ loops.js
@@ -13,10 +13,16 @@ for(var i = 0; i < estudiantes.length; i++){
 //se llama a esta función, que mostrará el saludo con el nombre del almuno segur la posición
 //del index basado en i
 }
-
 //al hacer la prueba en la consola del navegador, se muestra el mensaje escrito en el console.log
 //seguido del nombre del estudiante correspondiente
 //Hola, Lucy
 //Hola, Bety
 //Hola, Ale
 //Hola, Janie
+
+//Otra manera, mismo 
+//la variable "estudiante" es tomada como el singular de los elementos en el array "estudiantes"
+//convirtiendose en el index, el estudiante 0
+for (var estudiante of estudiantes){
+    saludarEstudiantes(estudiante); //se llama al index con la variable "estudiante"
+}
