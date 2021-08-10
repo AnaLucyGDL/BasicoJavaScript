df8f1398dcbf3265ae5de9fbbb32dc7156cdf18b
Lucy Guzmán <analucygdl@gmail.com>
2021-08-10 17:39:53 -0500

    Creación de objeto

---
+++ objects.js
@@ -0,0 +1,12 @@
+//Sintaxis: var objeto = {};
+//Al objeto se manejan con propiedades, y las propiedadss (la forma en que se pueden entender) es 
+//una combinación de palabras clave y valores
+
+//objeto con tres propiedades
+var carro = {
+    marca : "Toyota",
+    modelo: "Corolla",
+    anio : 2020 //la ñ es un caracter especial que en el lenguaje no se entiende
+};
+//se llama al objeto para que muestre sus propiedades
+carro;
