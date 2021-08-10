82176cc3ef310b0a4c8940e3277a14f464b618e4
Lucy Guzmán <analucygdl@gmail.com>
2021-08-10 01:08:45 -0500

    Adición de console.log para mejor comprensión

--- while.js
+++ while.js
@@ -6,6 +6,7 @@ function saludarEstudiantes(estudiante){
 }
 //Mientras el parametro sea verdad, la función se ejecuta
    while (estudiantes.length > 0) {
+       console.log(estudiante)
        var estudiante = estudiantes.shift();
 //se genera la variable estudiante que es igual al array con el metodo de mutación que estara sacando
 //elementos y manda a llamar la función con la variable de "estudiante" como parametro
