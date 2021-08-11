c1a652e85a34c6eb6a002a2480a1ad6c628cf4cc
Lucy Guzmán <analucygdl@gmail.com>
2021-08-11 00:53:23 -0500

    Creación de función constructora

---
+++ constructora.js
@@ -0,0 +1,9 @@
+//Creación de la función constructora
+               //propiedades que se esperan que tenga la "template"
+function auto (marca, modelo, anio){
+    this.marca = marca; 
+    //el this es una variable especifica, aquí se usa como referencia al objeto. Nuestro objeto en 
+    //este caso es la función "auto". Se referencia a la función constructora "auto" y la palabra
+    //clave "marca" que es igual a la propiedad del objeto.
+}
+//tengo mi función que tiene como parametro, las propiedades que espero que tenga mi función
