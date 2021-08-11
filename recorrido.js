a860940bc15c600e056d7cd08d36c376c6191d49
Lucy Guzmán <analucygdl@gmail.com>
2021-08-11 01:41:46 -0500

    Creación del array y su contenido (objetos)

---
+++ recorrido.js
@@ -0,0 +1,12 @@
+//Crear arrays conteniendo objetos, y como obtener propiedades especificas de los objetos dentro del array
+
+var articulos = [
+    {nombre: "Bici", costo: 3000},
+    {nombre: "TV", costo: 2500},
+    {nombre: "Libro", costo:320},
+    {nombre: "Celular", costo: 10000},
+    {nombre: "Laptop", costo: 20000},
+    {nombre: "Teclado", costo: 500},
+    {nombre: "Audifonos", costo: 1700},
+    //mientras más objetos se tengan, el filtrar se vuelve más "valioso" para ocupar estos metodos
+]
