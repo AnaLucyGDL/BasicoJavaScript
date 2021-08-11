18bd69d00b4be6019810e5d11b9622257254fa48
Lucy Guzmán <analucygdl@gmail.com>
2021-08-10 20:18:43 -0500

    Creación de un metodo (función como propiedad)

--- objects.js
+++ objects.js
@@ -6,8 +6,19 @@
 var carro = {
     marca : "Toyota",
     modelo: "Corolla",
-    anio : 2020 //la ñ es un caracter especial que en el lenguaje no se entiende
+    anio : 2020, //la ñ es un caracter especial que en el lenguaje no se entiende
+
+//Cuando se trabaja la estrucura del objeto, se puede hacer que una propiedad sea una función
+//que use propiedades dentro del objeto. Se le llama metodo de objeto
+    detalledelcarro : function(){
+        console.log(`Auto ${this.modelo} ${this.anio}`);
+    }
+
 };
+carro.detalledelcarro();
+//Se muestra en consola "Auto Corolla 2020"
+
+
 //se llama al objeto para que muestre sus propiedades
 carro;
 
