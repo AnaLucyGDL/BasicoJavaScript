8f176c187aa3b188203361d759ff74987cbfd7f7
Lucy Guzmán <analucygdl@gmail.com>
2021-08-11 01:26:52 -0500

    Prueba en consola y resultados

--- constructora.js
+++ constructora.js
@@ -23,9 +23,13 @@ auto;
 var autoNuevo = new auto ("Tesla", "Model 3", 2020);
 
 autoNuevo;
-//al introducirlo en ña consola del navegador, se muestra:
+//al introducirlo en la consola del navegador, se muestra:
 // auto {marca: "Tesla", modelo: "Model 3", anio: 2020}
 
 //Ejemplos de nuevos objetos que son estancias de nuestra función constructora
 var autoNuevo2 = new auto ("Tesla", "Model X", 2018);
 var autoNuevo3 = new auto ("Toyota", "Corolla", 2020);
+
+autoNuevo2; //auto {marca: "Tesla", modelo: "Model X", anio: 2018}
+autoNuevo3; //auto {marca: "Toyota", modelo: "Corolla", anio: 2020}
+//según las funciones que sean llamadas en consola, se mostrará lo correspondiente a cada una
